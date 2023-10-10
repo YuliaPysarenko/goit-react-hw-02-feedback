86af1347891e099c9d415a8fe1c7075273e73bb1
YuliaPysarenko <y.pisarenko@ukr.net>
2023-10-10 10:39:02 +0300

    homeWork

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -5,7 +5,7 @@ import shortid from 'shortid';
 
 const FeedbackOptions = ({options, onLeaveFeedback}) => {
     return <section>
-         {options.map(({ option }) => {
+         {options.map((option ) => {
             return <button key={shortid.generate()}
                 type="button"
                 name= {option}
